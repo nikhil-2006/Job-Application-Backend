429786b3ed28836c202f1829d168b1e73facd7b9
nikhil <jjnikhil113@gmail.com>
2026-04-10 16:40:05 +0530

    Initial commit

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,10 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
-    console.log("Connecting to MongoDB...");
-    await mongoose.connect(process.env.MONGO_URI as string);
-    console.log("MongoDB Connected 🚀");
-  } catch (error) {
-    console.error("DB Error:", error);
-    throw error;
-  }
+  
+mongoose.connect(process.env.MONGO_URI!)
+  .then(() => console.log("MongoDB connected ✅"))
+  .catch((err) => console.log(err));
 };
 
 export default connectDB;
